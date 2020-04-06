80cdc376024cfd64594202ff26fd35777aae6bcd
Juan Felipe Torres <jf.torresp@uniandes.edu.co>
2020-04-06 14:04:30 -0500

    front-end folder added

---
+++ front-end/src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <div className="Sidebar">
+    <h1>Your Teams</h1>
+    <button>Create a Team</button>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+    <hr/>
+  </div>
+);
+
+export default Sidebar;
