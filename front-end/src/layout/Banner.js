3fc3d519cd2aef8fb1903b72f5b5edf38a153612
Juan Felipe Torres <jf.torresp@uniandes.edu.co>
2020-04-19 17:33:35 -0500

    Login form hidden when user logs

--- front-end/src/layout/Banner.js
+++ front-end/src/layout/Banner.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "../components/Login";
 import "../index.css";
 
 const Banner = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetch("/profile")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.user) setUser(data.user);
+      });
+  }, []);
+
   return (
     <div className='Banner'>
-      <div className='row'>
-        <div className='col-4 login'>
-          <Login />
+      {!user ? (
+        <div className='row'>
+          <div className='col-4 login'>
+            <Login />
+          </div>
+          <div className='col-8 title'>
+            <h1>Welcome to INcolab, start innovating, creating and helping!</h1>
+          </div>
         </div>
-        <div className='col-8 title'>
-          <h1>Welcome to INcolab, start innovating, creating and helping!</h1>
+      ) : (
+        <div className='row'>
+          <div className='col-12 title'>
+            <h1 className='text-center'>
+              Welcome to INcolab, start innovating, creating and helping!
+            </h1>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
